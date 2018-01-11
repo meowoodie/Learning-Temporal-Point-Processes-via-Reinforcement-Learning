0c8ba812a3b0bfa437232a035671efd79865944e
meowoodie <woodielove@163.com>
2018-01-11 16:39:21 -0500

    almost finish rewards

--- imitpp.py
+++ imitpp.py
@@ -15,10 +15,16 @@ import tensorflow as tf
 
 class PointProcessGenerater(object):
 	"""
-	
+
+	For your information: 
+	action has shape [batch_size, feature_size]
+	state has shape  [batch_size, state_size]
 	"""
 
 	def __init__(self, seq_len=5, state_size=3, batch_size=2, feature_size=1):
+
+		tf.set_random_seed(100)
+		self.seq_len      = seq_len
 		self.batch_size   = batch_size
 		self.feature_size = feature_size
 		self.state_size   = state_size
@@ -26,31 +32,59 @@ class PointProcessGenerater(object):
 		# create a BasicRNNCell
 		self.rnn_cell = tf.contrib.rnn.BasicRNNCell(state_size)
 
-		# rnn_input has shape [batch_size, sequence_len, feature_size]
+		# input_data has shape [batch_size, sequence_len, feature_size]
 		self.input_data = tf.placeholder(tf.float32, shape=[batch_size, seq_len, feature_size])
+
 		# state has shape [batch_size, state_size]
 		self.initial_state = self.rnn_cell.zero_state(batch_size, dtype=tf.float32)
 
-		# 1. Generator always produce fixed length sequences
+		# Optimizer of generator
+		
 
+	def fixed_length_rnn(self, rnn_len=5):
+		"""
+		"""
+		# rnn always produce fixed length sequences
 		# iteration counter for fixed length generator
-		iter_counter = np.zeros(seq_len)
-		# final_state has shape [sequence_len, batch_size, state_size]
-		self.flen_actions, self.flen_states = tf.scan(
-			# last_res[0]: last action
-			# last_res[1]: last state
-			# x: element in input data
-			lambda last_res, _: self._dynamic_rnn_unit(last_res[0], last_res[1]), 
+		iter_counter = np.zeros(rnn_len)
+		# iterate over counter to generate fixed length sequence
+		actions, states = tf.scan(
+			# action_state[0]: last action
+			# action_state[1]: last state
+			lambda action_state, _: self._dynamic_rnn_unit(action_state[0], action_state[1]), 
 			# used as counter in this loop
 			iter_counter,
-			# Initial rnn_output and state
+			# Initial action and state
 			initializer=(np.zeros((self.batch_size, self.feature_size), dtype=np.float32), self.initial_state))
+		# return a list of actions and a list of states that contain all the actions and states 
+		# which are produced at each iteration step
+		# actions has shape [seq_len, batch_size, feature_size]
+		return actions, states
+		
+	def _reward(self, t, expert_actions, max_learner_len=10):
+		"""
+		expert_actions has shape [batch_size, sequence_len]
+		"""
+		learner_actions, learner_states = self.fixed_length_rnn(rnn_len=max_learner_len)
+		# get time (index 0 = first element of a feature vector) of last action of each expert sequence
+		expert_times    = expert_actions[:, :, 0]
+		max_times       = tf.reduce_max(expert_times, reduction_indices=[1])
+		# indices for action in batch of actions and 
+		# indices for time in max times list
+		action_indices  = tf.constant(np.array(range(self.batch_size)), tf.int32)
+		# get learner times from learner actions
+		learner_times   = tf.scan(
+			# Only take index 0 value of feature vector into consideration
+			# which means time of the action. 
+			lambda _, ind: tf.multiply(
+				tf.cast(learner_actions[:, ind, 0] < max_times[ind], tf.float32), 
+				learner_actions[:, ind, 0]),
+			action_indices,
+			initializer=np.zeros(self.seq_len, dtype=np.float32))
 
-		# 2. Generator always produced fixed time frame sequences 
 
-		max_t = tf.get_variable("max_t", [1])
-		cond  = lambda t: t < max_t
-		tf.while_loop()
+
+		return expert_times, learner_times
 
 	# Building Blocks of Computational Graph
 
@@ -62,33 +96,36 @@ class PointProcessGenerater(object):
 		rnn_input = tf.reshape(prv_action, [self.batch_size, 1, self.feature_size])
 
 		_, cur_state = tf.nn.dynamic_rnn(self.rnn_cell, rnn_input,
-												   initial_state=prv_state,
-												   dtype=tf.float32)
-
+										 initial_state=prv_state,
+										 dtype=tf.float32)
 		# Reparameterization trick
-        # Reyleigh distribution with parameter sigma.
+		# Reyleigh distribution with parameter sigma.
 		W = tf.get_variable("W_r", [self.state_size, self.feature_size])
 		b = tf.get_variable("b_r", [self.feature_size], initializer=tf.constant_initializer(0.0))
 		sigma = tf.nn.elu(tf.matmul(cur_state, W) + b) + 1
 		stoch_action = sigma * tf.sqrt(-2 * tf.log(tf.random_uniform(tf.shape(sigma), minval=0, maxval=1)))
 		cur_action = tf.add(prv_action, sigma)
-
-		return cur_action, cur_state	
-
-	def _reward(self, expert_seqs, learner_seqs):
-		"""
-		"""
+		# action has shape [batch_size, feature_size]
+		# state has shape  [batch_size, state_size]
+		return cur_action, cur_state
 
 	# Available Functions
 
-	def generate(self, sess, seq_len, pretrained=False):
+	def generate(self, sess, pretrained=False):
 		"""
 		"""
+		times, actions = self._reward(self.input_data, max_rnn_len=5)
+
 		if not pretrained:
 			init = tf.global_variables_initializer()
 			sess.run(init)
 
-		sess.run([self.actions, self.states], feed_dict={})
+		test_times, test_actions = sess.run([times, actions], feed_dict={
+			self.input_data: [[[1], [1.5], [2], [0], [0]], [[1.1], [1.2], [2.4], [3.3], [5]], [[2], [2.2], [2.5], [3], [0]]]
+			})
+
+		print test_times
+		print test_actions
 
 
 	def train(self, sess, expert_data, pretrained=False):
@@ -106,73 +143,19 @@ class PointProcessGenerater(object):
 
 
 if __name__ == "__main__":
-
-	batch_size   = 2
-	state_size   = 3
 	seq_len      = 5
-	feature_size = 7
-
-	tf.set_random_seed(100)
-
-	# # rnn input has shape [batch_size, sequence_len, feature_size]
-	# rnn_input = tf.placeholder(tf.float32, shape=[batch_size, seq_len, feature_size])
-
-	# # create a BasicRNNCell
-	# rnn_cell = tf.nn.rnn_cell.BasicRNNCell(state_size)
-
-	# # defining initial state
-	# initial_state = rnn_cell.zero_state(batch_size, dtype=tf.float32)
-
-	# # 'state' is a tensor of shape [batch_size, cell_state_size]
-	# # 'outputs' is a tensor of shape [batch_size, sequence_len, cell_state_size]
-	# rnn_outputs, state = tf.nn.dynamic_rnn(rnn_cell, rnn_input,
-	#                                        initial_state=initial_state,
-	#                                        dtype=tf.float32)
+	batch_size   = 3
+	state_size   = 4
+	feature_size = 1
 
 	with tf.Session() as sess:
-		
-		sess.run(tf.global_variables_initializer())
 
-		# test_output, test_state = sess.run(
-		# 	[rnn_outputs, state], 
-		# 	feed_dict={rnn_input: [[[1],[2],[3],[4],[5]], [[5],[4],[3],[2],[1]]]})
-
-		# imitpp = PointProcessGenerater(feature_size=feature_size)
-		# sess.run(imitpp.init)
-		# test_output, test_state = sess.run(
-		# 	[imitpp.actions, imitpp.states],
-		# 	feed_dict={imitpp.input_data: [
-		# 	[[1,1,1,1,1,1,1],
-		# 	 [2,2,2,2,2,2,2],
-		# 	 [3,3,3,3,3,3,3],
-		# 	 [4,4,4,4,4,4,4],
-		# 	 [5,5,5,5,5,5,5]], 
-		# 	[[1,1,1,1,1,1,1],
-		# 	 [2,2,2,2,2,2,2],
-		# 	 [3,3,3,3,3,3,3],
-		# 	 [4,4,4,4,4,4,4],
-		# 	 [5,5,5,5,5,5,5]]]})
-
-		# # [5, 2, 1, 3]
-		# print np.array(test_output)
-		# # [5, 2, 3]
-		# print np.array(test_state)
-
-		max_t = 4
-		cond = lambda t: t < max_t
-		body = lambda t: tf.add(t, 1)
-		r = tf.while_loop(cond, body, np.array([1,2,3,4,5]))
-		res = sess.run(r)
-		print res
-
-
-
-	
-
-
-
-
-
+		ppg = PointProcessGenerater(
+			seq_len=seq_len,
+			batch_size=batch_size, 
+			state_size=state_size, 
+			feature_size=feature_size)
+		ppg.generate(sess)
 
 
 
