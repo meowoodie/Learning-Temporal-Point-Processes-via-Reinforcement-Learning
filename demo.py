7aa43de6fbe57eb044d33f60621ce88e1f84a2b7
meowoodie <woodielove@163.com>
2018-01-12 16:31:10 -0500

    fix a bug for reward function

--- demo.py
+++ demo.py
@@ -29,5 +29,18 @@ if __name__ == "__main__":
 			state_size=state_size,
 			feature_size=feature_size)
 
-		ppg.train(sess, input_data)
-		ppg.generate(sess, num_seq=10, max_t=4, max_learner_len=10)
+		print ppg.train(sess, input_data)
+		# print ppg.generate(sess, num_seq=3, max_t=7, max_learner_len=10)
+
+		# A = tf.constant([[ 1.10000002], [ 1.20000005], [ 2.4000001 ], [ 3.29999995], [ 5.        ], [ 1.12912512], [ 1.60563707], [ 2.87550449], [ 3.63043833], [ 4.48129034]])
+		# r = tf.reduce_sum(A*A, 1)
+
+		# # turn r into column vector
+		# r = tf.reshape(r, [-1, 1])
+		# D = r - 2*tf.matmul(A, tf.transpose(A)) + tf.transpose(r)
+
+		# a = tf.constant([1,2,3])
+		# D = tf.transpose(tf.reshape(tf.tile(a, [2]), (2,3)))
+		# sess = tf.Session()
+		# print sess.run(D)
+
--- imitpp.py
+++ imitpp.py
@@ -19,8 +19,10 @@ class PointProcessGenerater(object):
 	"""
 
 	def __init__(self, seq_len=5, state_size=3, batch_size=2, feature_size=1):
+
 		#TODO: remove seq_len
 		tf.set_random_seed(100)
+
 		self.seq_len      = seq_len
 		self.batch_size   = batch_size
 		self.feature_size = feature_size
@@ -31,22 +33,6 @@ class PointProcessGenerater(object):
 
 		# input_data has shape [batch_size, sequence_len, feature_size]
 		self.input_data = tf.placeholder(tf.float32, shape=[batch_size, seq_len, feature_size])
-
-		# expert actions and corresponding learner actions
-		# expert_actions has shape [batch_size, seq_len, feature_size]
-		# learner_actions has shape [seq_len, batch_size, feature_size]
-		expert_actions     = self.input_data #TODO: replace input_data with a batch of input_data
-		learner_actions, _ = self._fixed_length_rnn(self.batch_size, rnn_len=self.seq_len)
-		# in order to avoid a specific bug (or flaw) triggerred by tensorflow itself
-		# here unfold matrix into a 1D list first, then apply reward function to every single element
-		# in the tensor, finally, refold the result back to a matrix with same shape with original one
-		unfold_times = tf.reshape(learner_actions[:, :, 0], [-1])
-		unfold_rewards = tf.map_fn(lambda t: self._reward(t, expert_actions, learner_actions), unfold_times)
-		refold_rewards = tf.reshape(unfold_rewards, [self.seq_len, self.batch_size])
-		# loss function
-		self.loss = tf.reduce_mean(tf.reduce_sum(refold_rewards, axis=0))
-		# optimizer
-		self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(self.loss)
 
 	# Building Blocks of Computational Graph
 
@@ -71,11 +57,13 @@ class PointProcessGenerater(object):
 			action_indices,
 			initializer=np.zeros(self.seq_len, dtype=np.float32))
 
+		# calculate kernel bandwidth
+		medians = self._median_pairwise_distance(expert_times, learner_times)
+		medians = tf.transpose(tf.reshape(tf.tile(medians, [self.seq_len]), (self.seq_len, self.batch_size)))
 		# return reward
-		#TODO: change batch_size to the median of pairwise distance
 		reward = tf.reduce_mean(tf.subtract(
-			tf.reduce_sum(self._gaussian_kernel(expert_times, t, self.batch_size), axis=1), \
-			tf.reduce_sum(self._gaussian_kernel(learner_times, t, self.batch_size), axis=1)))
+			tf.reduce_sum(self._gaussian_kernel(expert_times, t, medians), axis=1), \
+			tf.reduce_sum(self._gaussian_kernel(learner_times, t, medians), axis=1)))
 
 		return reward
 
@@ -96,7 +84,7 @@ class PointProcessGenerater(object):
 			iter_counter,
 			# Initial action and state
 			initializer=(np.zeros((num_seq, self.feature_size), dtype=np.float32), initial_state))
-		# return a list of actions and a list of states that contain all the actions and states 
+		# return a list of actions and a list of states that contain all the actions and states
 		# which are produced at each iteration step
 		# actions has shape [rnn_len, num_seq=batch_size, feature_size]
 		return actions, states
@@ -125,23 +113,74 @@ class PointProcessGenerater(object):
 		# state has shape  [num_seq, state_size]
 		return cur_action, cur_state
 
-	def _gaussian_kernel(self, real_t, var_t, const):
+	def _median_pairwise_distance(self, seqAs, seqBs):
+		"""
+
+		reference: https://stackoverflow.com/questions/37009647/compute-pairwise-distance-in-a-batch-without-replicating-tensor-in-tensorflow
+		"""
+		#TODO: remove the reshape process in the future. 
+		#      for now, the seqs has shape [batch_size, seq_len],
+		#      in the future, the calculation of distance could be applied to 
+		#      high dimensional data, which means the input seqs has shape [batch_size, seq_len, dim]
+		seq_len = tf.shape(seqAs)[1] + tf.shape(seqBs)[1]
+		seqs = tf.concat([seqAs, seqBs], axis=1)
+		seqs = tf.reshape(seqs, [self.batch_size, seq_len, 1])
+
+		medians = tf.scan(
+			lambda median, seq: self.__median(seq),
+			seqs,
+			# initializer=np.zeros(0, np.float32))
+			initializer=np.array(0, dtype=np.float32))
+
+		return medians
+
+	def _gaussian_kernel(self, real_ts, var_ts, consts):
 		"""
 		"""
 		# get mask of non zero value from real_t
-		mask = tf.cast(real_t > 0.0, dtype=tf.float32)
+		mask = tf.cast(real_ts > 0.0, dtype=tf.float32)
 		# put mask on var_t in order to cancel the padding value in the real_t
-		var_t = tf.multiply(var_t, mask)
+		var_ts = tf.multiply(var_ts, mask)
 
-		return tf.exp(- 1 * tf.square(real_t - var_t) / (2 * const))
+		return tf.exp(- 1 * tf.divide(tf.square(real_ts - var_ts), (2 * consts)))
+
+	@staticmethod
+	def __median(seq):
+		"""
+		"""
+		#TODO: mask should be handled carefully when dim is larger than 1. 
+		#      a good mask will recognize the valid cells within A. 
+		#      simply using A > 0 will not be able to be applied to high-dimensional cases.
+		# mask is a matrix which consist of 1 and -1, means valid and invalid cells respectively.
+		mask_col   = (tf.cast(seq > 0, dtype=tf.float32) - 0.5) * 2
+		mask_row   = tf.transpose(mask_col)
+		mask       = tf.matmul(mask_col, mask_row)
+		valid_num  = tf.reduce_sum(tf.cast(mask >= 0, dtype=tf.int32))
+		median_ind = tf.cast(valid_num/2, dtype=tf.int32)
+		# calculate pairwise distance of a single seq
+		r     = tf.reduce_sum(seq * seq, 1)
+		r     = tf.reshape(r, [-1, 1])
+		pdist = tf.reshape(   # flatten distanes into a 1D array
+				tf.multiply(  # apply mask: remove invalid distances
+					mask, (r - 2 * tf.matmul(seq, tf.transpose(seq)) + tf.transpose(r))), [-1]) 
+		median = tf.nn.top_k(pdist, median_ind).values[median_ind-1] + \
+			     tf.constant(1e-06, dtype=tf.float32) # in avoid of medians is 0
+
+		return median
 
 	# Available Functions
+	# Runnable Computational Graphs
 
 	def generate(self, sess, num_seq, max_t, max_learner_len=10, pretrained=False):
 		"""
 		"""
+
+		# Generator Computational Graph
+
 		actions, states = self._fixed_length_rnn(num_seq, rnn_len=max_learner_len)
 		times = tf.multiply(tf.cast(actions[:, :, 0] < max_t, tf.float32), actions[:, :, 0])
+
+		# Runnning Graph
 
 		if not pretrained:
 			init = tf.global_variables_initializer()
@@ -155,11 +194,86 @@ class PointProcessGenerater(object):
 		"""
 		"""
 
+		# Optimizer Computational Graph
+		# notes: putting optimizer graph outside of the init function since it would cause 
+		#        a conflict when you try to run the generate graph. Because the tensorflow
+		#        will try to compile a _fixed_length_rnn subgraph which has been initialized
+		#        in optimizer graph.
+
+		# expert actions and corresponding learner actions
+		# expert_actions has shape [batch_size, seq_len, feature_size]
+		# learner_actions has shape [seq_len, batch_size, feature_size]
+		expert_actions     = self.input_data #TODO: replace input_data with a batch of input_data
+		learner_actions, _ = self._fixed_length_rnn(self.batch_size, rnn_len=self.seq_len)
+		# in order to avoid a specific bug (or flaw) triggerred by tensorflow itself
+		# here unfold matrix into a 1D list first, then apply reward function to every single element
+		# in the tensor, finally, refold the result back to a matrix with same shape with original one
+		unfold_times = tf.reshape(learner_actions[:, :, 0], [-1])
+		unfold_rewards = tf.map_fn(lambda t: self._reward(t, expert_actions, learner_actions), unfold_times)
+		refold_rewards = tf.reshape(unfold_rewards, [self.seq_len, self.batch_size])
+		# # loss function
+		self.loss = tf.reduce_mean(tf.reduce_sum(refold_rewards, axis=0))
+		# # optimizer
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=1e-4, beta1=0.5, beta2=0.9).minimize(self.loss)
+
+		# Runnning Graph
+
 		# Set pretrained variable if it was existed
 		if not pretrained:
 			init = tf.global_variables_initializer()
 			sess.run(init)
 
+		# step        = 1 # the step of the iteration
+		# start_index = 0 # the index of the start row of the batch
+		# # Keep training until reach max iterations
+		# while step * self.batch_size <= self.iters:
+		# 	# Fetch the next batch of the input data (q, d, y)
+		# 	# And update the start_indext in order to prepare for the next batch
+		# 	batch_qs, batch_ds, batch_ys, start_index = self._next_batch(qs, ds, ys, start_index)
+		# 	# Run optimization
+		# 	sess.run(self.optimizer, feed_dict={self.q: batch_qs, self.d: batch_ds, self.y: batch_ys})
+		# 	if step % self.display_step == 0:
+		# 		# Calculate batch loss and accuracy
+		# 		loss, acc = sess.run(
+		# 			[self.cost, self.accuracy], 
+		# 			feed_dict={self.q: batch_qs, self.d: batch_ds, self.y: batch_ys})
+		# 		test_loss, test_acc = sess.run(
+		# 			[self.cost, self.accuracy],
+		# 			feed_dict={self.q: test_qs, self.d: test_ds, self.y: test_ys})
+		# 		# Log information for each iteration
+		# 		print >> sys.stderr, "[%s] Iter: %d" % (arrow.now(), (step * self.batch_size)) 
+		# 		print >> sys.stderr, "[%s] Train:\tloss (%.5f)\tacc (%.5f)" % (arrow.now(), loss, acc)
+		# 		print >> sys.stderr, "[%s] Test:\tloss (%.5f)\tacc (%.5f)" % (arrow.now(), test_loss, test_acc)
+		# 		# print >> sys.stderr, "[%s] Test node value:", nodes[0].shape
+		# 	step += 1
+		# print >> sys.stderr, "[%s] Optimization Finished!" % arrow.now()
+
 		#TODO: Do training process in batch
-		sess.run(self.optimizer, 
+		return sess.run(self.optimizer, 
 			feed_dict={self.input_data: input_data})
+
+	def _next_batch(self, input_data, start_index):
+		"""
+		Next Batch
+		
+		This is a private method for fetching a batch of data from the integral input data. 
+		Each time you call this method, it would return the next batch in the dataset by indicating 
+		the start index. Which means you have to keep the return start index of every invoking,
+		and pass it to the next invoking for getting the next batch correctly. 
+		"""
+
+		# total number of rows of the input data (query and target)
+		num_row, num_col = np.shape(input_data)   
+		# start index of the row of the input data
+		start_row = start_index % num_row 
+		# end index of the row of the input data
+		end_row   = (start_row + self.batch_size) % num_row 
+		# if there is not enought data left in the dataset for generating an integral batch,
+		# then top up this batch by traversing back to the start of the dataset. 
+		if end_row < start_row:
+			batch_input_data = np.append(input_data[start_row: num_row], input_data[0: end_row], axis=0).astype(np.float32)
+		else:
+			batch_input_data = input_data[start_row: end_row].astype(np.float32)
+		# Update the start index
+		start_index += self.batch_size
+		return batch_input_data, start_index
