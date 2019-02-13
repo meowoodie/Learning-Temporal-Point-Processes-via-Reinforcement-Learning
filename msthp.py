79159c5605bff30cf44727fd309621c68e20a6df
Shixiang Zhu <woodie@lawn-128-61-58-9.lawn.gatech.edu>
2019-02-12 21:46:54 -0500

    minor update

--- msthp.py
+++ msthp.py
@@ -41,64 +41,44 @@ class MarkedSpatialTemporalHawkesProcess(object):
     A stochastic marked spatial temporal points generator based on Hawkes process.
     """
 
-    def __init__(self, kernel, mu=0, alpha=1., beta=1., max_lam=500., m_dim=None):
+    def __init__(self, kernel, step_size, mu=0, alpha=1., beta=1., max_lam=500., m_dim=None):
         """
         Params:
         - T:     maximum time
         - m_dim: number of categories of marks
         """
-        self.mu      = tf.get_variable(name="lam_mu", initializer=tf.constant(mu))
-        self.alpha   = tf.get_variable(name="lam_alpha", initializer=tf.constant(alpha))
-        self.beta    = tf.get_variable(name="lam_beta", initializer=tf.constant(beta))
-        self.max_lam = max_lam
-        self.kernel  = kernel
+        # model parameters
+        self.mu        = tf.get_variable(name="lam_mu", initializer=tf.constant(mu))
+        self.alpha     = tf.get_variable(name="lam_alpha", initializer=tf.constant(alpha))
+        self.beta      = tf.get_variable(name="lam_beta", initializer=tf.constant(beta))
+        self.max_lam   = max_lam
+        self.kernel    = kernel
+        self.step_size = step_size
+        # generated samples
+        self.seq_t   = []
+        self.seq_l   = []
+        # sampling process
+        self._homogeneous_sampling(T=10.0, x_lim=[0, 1], y_lim=[0, 1], batch_size=2)
 
-    def _lam_value(self, seq_t, seq_s):
+    def _lam_value(self, seq_t, seq_l):
         '''
-        return the intensity value at (t, s).
-        The last element of seq_t and seq_s is the location (t, s) that we are
+        return the intensity value at (t, l).
+        The last element of seq_t and seq_s is the location (t, l) that we are
         going to inspect. Prior to that are the past locations which have
         occurred.
         '''
+        seq_t = tf.stack(self.seq_t, axis=1) # [batch_size, step_size, 1]
+        seq_l = tf.stack(self.seq_l, axis=1) # [batch_size, step_size, 2]
         if tf.shape(seq_t)[0] > 1:
             # get current time, spatial values and historical time, spatial values.
             cur_t, his_t = seq_t[-1], seq_t[:-1]
-            cur_s, his_s = seq_s[-1], seq_s[:-1]
-            val = self.mu + self.alpha * tf.reduce_sum(self.beta * self.kernel.nu(cur_t-his_t, cur_s-his_s))
+            cur_l, his_l = seq_l[-1], seq_l[:-1]
+            val = self.mu + self.alpha * tf.reduce_sum(self.beta * self.kernel.nu(cur_t-his_t, cur_l-his_l))
         else:
             val = self.mu
         return val
 
-    def _lam_upper_bound(self):
-        '''return the upper bound of the intensity value'''
-        return self.max_lam
-
-    # def _homogeneous_samples(self, T, x_lim, y_lim, batch_size):
-    #     '''
-    #     To generate a homogeneous Poisson point pattern in space S, this function
-    #     uses a two step procedure:
-    #     1. Simulate the number of events n = N(S) occurring in S according to a
-    #     Poisson distribution with mean lam * |S|.
-    #     2. Sample each of the n location according to a uniform distribution on S
-    #     respectively.
-    #     Returns samples: point process samples  
-    #         [(t1, x1, y1), (t2, x2, y2), ..., (tn, xn, yn)]
-    #     '''
-    #     # sample space
-    #     S  = [[0, T], x_lim, y_lim]
-    #     # calculate the number of events in space S
-    #     n  = utils.lebesgue_measure(S)
-    #     Ns = tf.random_poisson([batch_size], lam=self._lam_upper_bound() * n, dtype=tf.int32)
-    #     # simulate spatial sequence and temporal sequence separately.
-    #     seq_ts = tf.random_uniform([batch_size, tf.reduce_max(Ns)], minval=0, maxval=T, dtype=tf.float32)
-    #     seq_xs = tf.random_uniform([batch_size, tf.reduce_max(Ns)], minval=x_lim[0], maxval=x_lim[1], dtype=tf.float32)
-    #     seq_ys = tf.random_uniform([batch_size, tf.reduce_max(Ns)], minval=y_lim[0], maxval=y_lim[1], dtype=tf.float32)
-    #     seq_ls = tf.stack([seq_xs, seq_ys], axis=1)
-    #     # sort the sequence regarding the ascending order of the temporal sample.
-    #     seq_ts = tf.contrib.framework.sort(seq_ts, axis=0, direction='ASCENDING')
-    #     return seq_ts, seq_ls, Ns
-
-    def generate(self, T, x_lim, y_lim, batch_size):
+    def _homogeneous_sampling(self, T, x_lim, y_lim, batch_size):
         '''
         To generate a homogeneous Poisson point pattern in space S, this function
         uses a two step procedure:
@@ -109,16 +89,23 @@ class MarkedSpatialTemporalHawkesProcess(object):
         Returns samples: point process samples 
             [(t1, x1, y1), (t2, x2, y2), ..., (tn, xn, yn)]
         '''
-        t         = tf.constant(0, dtype=tf.float32)
-        loop_vars = [t]
-        cond      = lambda t: tf.less(t, T) # termint < T
-        body      = lambda t: self.__sampling_point(t)
-        return tf.while_loop(cond, body, loop_vars)
+        # t         = tf.constant(0, dtype=tf.float32)  # initial time
+        # loop_vars = [t]                               # loop variables
+        # cond      = lambda t: tf.less(t, T)           # termination condition of the loop
+        # body      = lambda t: self._sampling_point(t)
+        # tf.while_loop(cond, body, loop_vars)
+        for _ in range(len(self.step_size)):
+            lam_hat = self._lam_value(self.seq_t, self.seq_l)
 
-    @staticmethod
-    def __sampling_point(t):
-        return tf.add(t, 1)
-
+    def _sampling_point(self, t):
+        # lam_hat = self._lam_value(self.seq_t, self.seq_l)
+        # # generate sample candidate
+        # u = tf.random_uniform([1], minval=0, maxval=1, dtype=tf.float32)
+        # w = -tf.log(u) / lam_hat  # so that w ~ exponential(lam_hat)
+        # t = t + w                 # so that t is the next candidate point
+        # # thinning sample
+        # d = tf.random_uniform([1], minval=0, maxval=1, dtype=tf.float32) # acceptance rate
+        # tf.cond(tf.less(d * lam_hat, self._lam_value(self.seq_t, self.seq_l)), )
 
         
 if __name__ == "__main__":
@@ -127,5 +114,5 @@ if __name__ == "__main__":
     
     tf.set_random_seed(1)
     with tf.Session() as sess:
-        points = sess.run(hp.generate(T=10.0, x_lim=[0, 1], y_lim=[0, 1], batch_size=2))
+        points = sess.run(tf.random_uniform([], minval=0, maxval=1, dtype=tf.float32))
         print(points)
