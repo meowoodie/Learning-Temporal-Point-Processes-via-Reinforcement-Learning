7ccc2703cc832a86dd9867ec31581c72757ebe74
meowoodie <woodielove@163.com>
2019-04-02 01:55:02 -0400

    add plotting tool for visualizing the spatial intensity on the map

--- demo.py
+++ demo.py
@@ -7,11 +7,13 @@ Demo for testing basic version of imitpp on synthetic dataset
 
 import sys
 import arrow
+import utils
 import random
 import numpy as np
 import tensorflow as tf
 
 from ppgrl import RL_Hawkes_Generator
+from stppg import HawkesLam, GaussianMixtureDiffusionKernel
 
 # Avoid error msg [OMP: Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized.]
 # Reference: https://github.com/dmlc/xgboost/issues/1715
@@ -19,24 +21,25 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 if __name__ == "__main__":
-	# generate expert sequences
-	# np.random.seed(0)
-	# tf.set_random_seed(1)
+	data   = np.load('../Spatio-Temporal-Point-Process-Simulator/data/apd.crime.perday.npy')
+	params = np.load('../Spatio-Temporal-Point-Process-Simulator/data/gaussian_mixture_params.npz')
 
-	expert_seqs = np.load('../Spatio-Temporal-Point-Process-Simulator/results/free_hpp_Mar_10.npy')
-	expert_seqs = expert_seqs[:200, :, :]
-	print(expert_seqs.shape)
+	da     = utils.DataAdapter(init_data=data)
+	mu     = params['mu']
+	kernel = GaussianMixtureDiffusionKernel(
+		n_comp=5, layers=[5], C=1., beta=params['beta'], 
+		SIGMA_SHIFT=.05, SIGMA_SCALE=.2, MU_SCALE=.1,
+		Wss=params['Wss'], bss=params['bss'], Wphis=params['Wphis'])
+	lam    = HawkesLam(mu, kernel, maximum=1e+3)
 
-	# training model
-	with tf.Session() as sess:
-		# model configuration
-		batch_size = 20
-		epoches    = 15
-		lr         = 1e-5
-		T          = [0., 10.]
-		S          = [[-1., 1.], [-1., 1.]]
-		layers     = [20, 20]
+	# remove the first element in the seq, since t_0 is always 0, 
+	# which will cause numerical issue when computing lambda value
+	seqs   = da.normalize(data)[:, 1:, :] 
+	seq    = seqs[0]
+	print(seq[0:50]) 
 
-		ppg = RL_Hawkes_Generator(T=T, S=S, layers=layers, batch_size=batch_size, 
-			C=1., maximum=1e+3, keep_latest_k=None, lr=lr, eps=0)
-		ppg.train(sess, epoches, expert_seqs, trainplot=False)
+	# ngrid should be smaller than 100, due to the computational 
+	# time is too large when n > 100.
+	utils.spatial_intensity_on_map(
+		"test.html", da, lam, seq, t=.2, 
+		xlim=da.xlim, ylim=da.ylim, ngrid=100)
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import branca
+import folium
+import geopandas
+import pandas as pd
 import numpy as np
 import seaborn as sns
 import tensorflow as tf
@@ -10,6 +13,7 @@ import matplotlib.cm as cm
 from matplotlib import animation
 from matplotlib.backends.backend_pdf import PdfPages
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from shapely.geometry import Polygon
 
 def lebesgue_measure(S):
     """
@@ -19,6 +23,8 @@ def lebesgue_measure(S):
     """
     sub_lebesgue_ms = [ sub_space[1] - sub_space[0] for sub_space in S ]
     return np.prod(sub_lebesgue_ms)
+
+
 
 def l2_norm(x, y):
     """
@@ -32,6 +38,8 @@ def l2_norm(x, y):
     xy    = tf.matmul(x, tf.transpose(y))               # [length, length]
     dist_mat = x_sqr + tf.transpose(y_sqr) - 2 * xy
     return dist_mat
+
+
 
 class Meter(object):
     """
@@ -47,6 +55,8 @@ class Meter(object):
         self.ax_l1      = self.fig_l.add_subplot(1,2,1)
         self.ax_l2      = self.fig_l.add_subplot(1,2,2)
         plt.ion()
+
+
     
 class PointProcessDistributionMeter(Meter):
     """
@@ -149,74 +159,125 @@ class PointProcessIntensityMeter(Meter):
 
 
 
-def plot_spatial_kernel(path, kernel, S, grid_size, 
-        sigma_x_clim=[0.1, 0.35], sigma_y_clim=[0.1, 0.35], rho_clim=[-1, 1]):
+class DataAdapter():
     """
-    Plot spatial kernel parameters over the spatial region, including 
-    sigma_x, sigma_x, and rho. 
+    A helper class for normalizing and restoring data to the specific data range.
+    
+    init_data: numpy data points with shape [batch_size, seq_len, 3] that defines the x, y, t limits
+    S:         data spatial range. eg. [[-1., 1.], [-1., 1.]]
+    T:         data temporal range.  eg. [0., 10.]
     """
-    assert len(S) == 2, '%d is an invalid dimension of the space.' % len(S)
-    # define the span for space region
-    x_span = np.linspace(S[0][0], S[0][1], grid_size+1)[:-1]
-    y_span = np.linspace(S[1][0], S[1][1], grid_size+1)[:-1]
-    # map initialization
-    sigma_x_map = np.zeros((grid_size, grid_size))
-    sigma_y_map = np.zeros((grid_size, grid_size))
-    rho_map     = np.zeros((grid_size, grid_size))
-    # grid entris calculation
-    for x_idx in range(grid_size):
-        for y_idx in range(grid_size):
-            s                         = [x_span[x_idx], y_span[y_idx]]
-            sigma_x, sigma_y, rho     = kernel.nonlinear_mapping(s)
-            sigma_x_map[x_idx][y_idx] = sigma_x
-            sigma_y_map[x_idx][y_idx] = sigma_y
-            rho_map[x_idx][y_idx]     = rho
-    # plotting
-    plt.rc('text', usetex=True)
-    plt.rc("font", family="serif")
-    # plot as a pdf file
-    with PdfPages(path) as pdf:
-        fig, axs = plt.subplots(1, 3)
-        cmap     = matplotlib.cm.get_cmap('viridis')
-        im_0     = axs[0].imshow(sigma_x_map, interpolation='nearest', origin='lower', cmap=cmap)
-        im_1     = axs[1].imshow(sigma_y_map, interpolation='nearest', origin='lower', cmap=cmap)
-        im_2     = axs[2].imshow(rho_map, interpolation='nearest', origin='lower', cmap=cmap)
-        print(sigma_x_map.min(), sigma_x_map.max())
-        print(sigma_y_map.min(), sigma_y_map.max())
-        print(rho_map.min(), rho_map.max())
-        # ticks for colorbars
-        im_0.set_clim(*sigma_x_clim)
-        im_1.set_clim(*sigma_y_clim)
-        im_2.set_clim(*rho_clim)
-        tick_0 = np.linspace(sigma_x_clim[0], sigma_x_clim[1], 5).tolist()
-        tick_1 = np.linspace(sigma_y_clim[0], sigma_y_clim[1], 5).tolist()
-        tick_2 = np.linspace(rho_clim[0], rho_clim[1], 5).tolist()
-        # set x, y labels for subplots
-        axs[0].set_xlabel(r'$x$')
-        axs[1].set_xlabel(r'$x$')
-        axs[2].set_xlabel(r'$x$')
-        axs[0].set_ylabel(r'$y$')
-        axs[1].set_ylabel(r'$y$')
-        axs[2].set_ylabel(r'$y$')
-        # remove x, y ticks
-        axs[0].get_xaxis().set_ticks([])
-        axs[1].get_xaxis().set_ticks([])
-        axs[2].get_xaxis().set_ticks([])
-        axs[0].get_yaxis().set_ticks([])
-        axs[1].get_yaxis().set_ticks([])
-        axs[2].get_yaxis().set_ticks([])
-        # set subtitle for subplots
-        axs[0].set_title(r'$\sigma_x$')
-        axs[1].set_title(r'$\sigma_y$')
-        axs[2].set_title(r'$\rho$')
-        # plot colorbar
-        cbar_0 = fig.colorbar(im_0, ax=axs[0], ticks=tick_0, fraction=0.046, pad=0.08, orientation="horizontal")
-        cbar_1 = fig.colorbar(im_1, ax=axs[1], ticks=tick_1, fraction=0.046, pad=0.08, orientation="horizontal")
-        cbar_2 = fig.colorbar(im_2, ax=axs[2], ticks=tick_2, fraction=0.046, pad=0.08, orientation="horizontal")
-        # set font size of the ticks of the colorbars
-        cbar_0.ax.tick_params(labelsize=5) 
-        cbar_1.ax.tick_params(labelsize=5) 
-        cbar_2.ax.tick_params(labelsize=5) 
-        # adjust the width of the gap between subplots
-        plt.subplots_adjust(wspace=0.2)
-        pdf.savefig(fig)
+    def __init__(self, init_data, S=[[-1, 1], [-1, 1]], T=[0., 10.]):
+        self.data = init_data
+        self.T    = T
+        self.S    = S
+        self.tlim = [ init_data[:, :, 0].min(), init_data[:, :, 0].max() ]
+        mask      = np.nonzero(init_data[:, :, 0])
+        x_nonzero = init_data[:, :, 1][mask]
+        y_nonzero = init_data[:, :, 2][mask]
+        self.xlim = [ x_nonzero.min(), x_nonzero.max() ]
+        self.ylim = [ y_nonzero.min(), y_nonzero.max() ]
+        print(self.tlim)
+        print(self.xlim)
+        print(self.ylim)
+
+    def normalize(self, data):
+        """normalize batches of data points to the specified range"""
+        rdata = np.copy(data)
+        for b in range(len(rdata)):
+            # scale x
+            rdata[b, np.nonzero(rdata[b, :, 0]), 1] = \
+                (rdata[b, np.nonzero(rdata[b, :, 0]), 1] - self.xlim[0]) / \
+                (self.xlim[1] - self.xlim[0]) * (self.S[0][1] - self.S[0][0]) + self.S[0][0]
+            # scale y
+            rdata[b, np.nonzero(rdata[b, :, 0]), 2] = \
+                (rdata[b, np.nonzero(rdata[b, :, 0]), 2] - self.ylim[0]) / \
+                (self.ylim[1] - self.ylim[0]) * (self.S[1][1] - self.S[1][0]) + self.S[1][0]
+            # scale t 
+            rdata[b, np.nonzero(rdata[b, :, 0]), 0] = \
+                (rdata[b, np.nonzero(rdata[b, :, 0]), 0] - self.tlim[0]) / \
+                (self.tlim[1] - self.tlim[0]) * (self.T[1] - self.T[0]) + self.T[0]
+        return rdata
+
+    def restore(self, data):
+        """restore the normalized batches of data points back to their real ranges."""
+        ndata = np.copy(data)
+        for b in range(len(ndata)):
+            # scale x
+            ndata[b, np.nonzero(ndata[b, :, 0]), 1] = \
+                (ndata[b, np.nonzero(ndata[b, :, 0]), 1] - self.S[0][0]) / \
+                (self.S[0][1] - self.S[0][0]) * (self.xlim[1] - self.xlim[0]) + self.xlim[0]
+            # scale y
+            ndata[b, np.nonzero(ndata[b, :, 0]), 2] = \
+                (ndata[b, np.nonzero(ndata[b, :, 0]), 2] - self.S[1][0]) / \
+                (self.S[1][1] - self.S[1][0]) * (self.ylim[1] - self.ylim[0]) + self.ylim[0]
+            # scale t 
+            ndata[b, np.nonzero(ndata[b, :, 0]), 0] = \
+                (ndata[b, np.nonzero(ndata[b, :, 0]), 0] - self.T[0]) / \
+                (self.T[1] - self.T[0]) * (self.tlim[1] - self.tlim[0]) + self.tlim[0]
+        return ndata
+
+    def normalize_location(self, x, y):
+        """normalize a single data location to the specified range"""
+        _x = (x - self.xlim[0]) / (self.xlim[1] - self.xlim[0]) * (self.S[0][1] - self.S[0][0]) + self.S[0][0]
+        _y = (y - self.ylim[0]) / (self.ylim[1] - self.ylim[0]) * (self.S[1][1] - self.S[1][0]) + self.S[1][0]
+        return np.array([_x, _y])
+
+
+
+def spatial_intensity_on_map(
+    path, # html saving path
+    da,   # data adapter object defined in utils.py
+    lam,  # lambda object defined in stppg.py
+    data, # a sequence of data points [seq_len, 3] happened in the past
+    t,    # observation moment (t)
+    xlim, # observation x range
+    ylim, # observation y range
+    ngrid=100):
+    """Plot spatial intensity at time t over the entire map given its coordinates limits."""
+    # data preparation
+    seq_t, seq_s = data[:, 0], data[:, 1:]
+    sub_seq_t = seq_t[seq_t < t]
+    sub_seq_s = seq_s[:len(sub_seq_t)]
+    # generate spatial grid polygons
+    xmin, xmax, width       = xlim[0], xlim[1], xlim[1] - xlim[0] 
+    ymin, ymax, height      = ylim[0], ylim[1], ylim[1] - ylim[0]
+    grid_height, grid_width = height / ngrid, width / ngrid
+    x_left_origin   = xmin
+    x_right_origin  = xmin + grid_width
+    y_top_origin    = ymax
+    y_bottom_origin = ymax - grid_height
+    polygons        = [] # spatial polygons
+    lam_dict        = {} # spatial intensity
+    _id             = 0
+    for i in range(ngrid):
+        y_top    = y_top_origin
+        y_bottom = y_bottom_origin
+        for j in range(ngrid):
+            # append the intensity value to the list
+            s = da.normalize_location((x_left_origin + x_right_origin) / 2., (y_top + y_bottom) / 2.)
+            v = lam.value(t, sub_seq_t, s, sub_seq_s)
+            lam_dict[str(_id)] = 0 if np.isnan(v) else v
+            _id += 1
+            # append polygon to the list
+            polygons.append(Polygon(
+                [(y_top, x_left_origin), (y_top, x_right_origin), (y_bottom, x_right_origin), (y_bottom, x_left_origin)])) 
+            # update coordinates
+            y_top    = y_top - grid_height
+            y_bottom = y_bottom - grid_height
+        x_left_origin  += grid_width
+        x_right_origin += grid_width
+    # convert polygons to geopandas object
+    geo_df   = geopandas.GeoSeries(polygons) 
+    # TODO: Add marks for the past events
+    # plot polygons on the map
+    map = folium.Map(location=[sum(xlim)/2., sum(ylim)/2.], zoom_start=12, zoom_control=True)
+    cm  = branca.colormap.linear.YlOrRd_09.scale(min(lam_dict.values()), max(lam_dict.values())) 
+    folium.GeoJson(
+        data = geo_df.to_json(),
+        style_function = lambda feature: {
+            'fillColor':   cm(lam_dict[feature['id']]),
+            'fillOpacity': .5,
+            'weight':      0.}     
+    ).add_to(map)
+    map.save(path)
